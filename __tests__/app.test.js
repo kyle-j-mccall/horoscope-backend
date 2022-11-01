d25c83534f8ecacc357ae516aff7d73095dcd4ae
Kyle McCall <kylem147@gmail.com>
2022-11-01 14:55:03 -0700

    zodiac detail route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,6 @@ const app = require('../lib/app');
 
 const { zodiacs } = require('../lib/zodiac-data');
 
-// describe('example test - you should probably update me', () => {
-//   it('home route should return hello world', async () => {
-//     const resp = await request(app).get('/');
-//     expect(resp.text).toEqual('hello world!');
-//   });
-// });
-
 describe('zodiac routes', () => {
   it('/zodiac should return a list of zodiacs', async () => {
     const res = await request(app).get('/zodiacs');
@@ -17,5 +10,18 @@ describe('zodiac routes', () => {
       return { id: zodiac.id, name: zodiac.name };
     });
     expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiac/:id should return zodiac details', async () => {
+    console.log('hello?');
+    const res = await request(app).get('/zodiacs/1');
+    console.log(res);
+    const aquarius = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(aquarius);
   });
 });
