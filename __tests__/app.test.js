fb539a0c0e808f678ea259e4cd750e1d3de0bdaf
Kyle McCall <kylem147@gmail.com>
2022-11-01 14:25:48 -0700

    zodiacs route test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,9 +1,21 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('example test - you should probably update me', () => {
-  it('home route should return hello world', async () => {
-    const resp = await request(app).get('/');
-    expect(resp.text).toEqual('hello world!');
+const { zodiacs } = require('../lib/zodiac-data');
+
+// describe('example test - you should probably update me', () => {
+//   it('home route should return hello world', async () => {
+//     const resp = await request(app).get('/');
+//     expect(resp.text).toEqual('hello world!');
+//   });
+// });
+
+describe('zodiac routes', () => {
+  it('/zodiac should return a list of zodiacs', async () => {
+    const res = await request(app).get('/zodiacs');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
   });
 });
